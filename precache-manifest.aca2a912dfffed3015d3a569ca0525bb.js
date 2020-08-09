537f35b8fe73ff5284aead0c8fd971c61b46c1b5
Jason Jafari <me@jasonjafari.com>
2020-08-09 14:52:35 -0400

    Updates

---
+++ precache-manifest.aca2a912dfffed3015d3a569ca0525bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18fef2e5571e083941d8cd267853a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e71da1026860d492031a",
+    "url": "/static/css/main.79c14122.chunk.css"
+  },
+  {
+    "revision": "7455d4ebd47129c690a9",
+    "url": "/static/js/2.8548da06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8548da06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71da1026860d492031a",
+    "url": "/static/js/main.ff0e4b00.chunk.js"
+  },
+  {
+    "revision": "9d402891ec11b629928c",
+    "url": "/static/js/runtime-main.28a2a7d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hacker-news-react/precache-manifest.8472c8a614b5966043a1195b11392add.js"
+  "/precache-manifest.aca2a912dfffed3015d3a569ca0525bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hacker-news-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
