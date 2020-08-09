43fde99e0a3cde89104d35b6938a64381d82423b
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:52:25 -0400

    Make and Add Loader on App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.scss';
 import Header from './components/Header'; 
 import News from './components/News'; 
 import { NewProvider } from './shared/context/new-context'; 
+import Loader from './components/Loader'; 
  
 const App = () => { 
   return ( 
@@ -11,6 +12,7 @@ const App = () => {
       <div className="App"> 
         <Header /> 
         <News /> 
+        <Loader /> 
       </div> 
     </NewProvider> 
   ); 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+ 
+import './Loader.scss' 
+ 
+const Loader = () => { 
+    return ( 
+        <section className="news loader-main"> 
+            <div className="loader"></div> 
+        </section> 
+    ); 
+} 
+ 
+export default Loader;
