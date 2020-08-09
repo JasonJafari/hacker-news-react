f99aa6f296838958450c5f11e96ef66e48c5b63f
Jason Jafari <me@jasonjafari.com>
2020-08-09 14:27:21 -0400

    Updates

---
+++ precache-manifest.a0bd7a4781bc7485640d7ddbbd90aadb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd5e41797c9ee5177391c5e2b33a884",
+    "url": "/hacker-news-react/index.html"
+  },
+  {
+    "revision": "a1fdbc91012c069ab895",
+    "url": "/hacker-news-react/static/css/main.8aa666df.chunk.css"
+  },
+  {
+    "revision": "7455d4ebd47129c690a9",
+    "url": "/hacker-news-react/static/js/2.8548da06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news-react/static/js/2.8548da06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1fdbc91012c069ab895",
+    "url": "/hacker-news-react/static/js/main.3dc30d07.chunk.js"
+  },
+  {
+    "revision": "3716a50f494772172830",
+    "url": "/hacker-news-react/static/js/runtime-main.a616473f.js"
+  }
+]);
