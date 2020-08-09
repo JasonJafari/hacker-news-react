9dfdbcd4bb13b1ac03607a345ab150c493413889
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:29:21 -0400

    Use News provide for app

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 import './App.scss'; 
 import Header from './components/Header'; 
 import News from './components/News'; 
+import { NewProvider } from './shared/context/new-context'; 
  
 const App = () => { 
   return ( 
-    <div className="App"> 
-      <Header /> 
-      <News /> 
-    </div> 
-   ); 
+    <NewProvider > 
+      <div className="App"> 
+        <Header /> 
+        <News /> 
+      </div> 
+    </NewProvider> 
+  ); 
 } 
  
 export default App; 
