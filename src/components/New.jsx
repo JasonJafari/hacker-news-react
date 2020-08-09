fc9749b1812145e2637234e8183dbda591bafe61
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:31:38 -0400

    make New jsx work

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,20 +2,18 @@ import React from 'react';
  
 import './New.scss' 
  
-const New = () => { 
+const New = (props) => { 
     return ( 
         <div className="new"> 
-            <h2>Stephen Hawking has died</h2> 
+            <h2>{props.title}</h2> 
             <ul> 
-                <li className="author">Author: Cogito</li> 
-                <li className="score">Score: 6015</li> 
-                <li><time datetime="2018-03-14T03:50:30.000Z"> created at :2018-03-14T03:50:30.000Z  </time></li> 
-                <li><a href="#">Source</a></li> 
+                <li className="author">Author: {props.author}</li> 
+                <li className="score">Score: {props.score}</li> 
+                <li> created at :{props.time}</li> 
+                <li><a href={props.link}>Source</a></li> 
             </ul> 
             <p> 
-                abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP. 
+                {props.Summary} 
             </p> 
         </div> 
     ); 
