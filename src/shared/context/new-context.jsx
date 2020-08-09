4294bbd6d4ea0ad405ff709f4b033e9fbdeff39d
Jason Jafari <me@jasonjafari.com>
2020-08-09 14:01:13 -0400

    change context to empyy first state

--- src/shared/context/new-context.jsx
+++ src/shared/context/new-context.jsx
@@ -3,17 +3,7 @@ import React, { createContext, useState } from 'react';
 export const NewContext = createContext(); 
  
 export const NewProvider = (props) => { 
-    const [getNewItems, setNewItems] = useState([ 
-        { 
-            id: 1, 
-            title: "Stephen Hawking has died", 
-            author: "Cogito", 
-            score: 6015, 
-            time: "2018-03-14T03:50:30.000Z", 
-            link: "hhtp://", 
-            Summary: null  
-        } 
-    ]) 
+    const [getNewItems, setNewItems] = useState([{}]) 
     return ( 
         <NewContext.Provider 
             value={[getNewItems, setNewItems]} 
