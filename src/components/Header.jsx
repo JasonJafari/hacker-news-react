cdf9f6d3aaf7cb0f9aaab5e2c4671f8a1b57446d
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:43:04 -0400

    change Header component for add API on it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,36 @@ const Header = () => {
     const handleSearch = (ev) => { 
         clearTimeout(timer); 
         let searchVlue = ev.target.value; 
-        timer = setTimeout(() => { 
-            setNewItems([{ id: 2 }, { id: 3 }, { id: 7 }]) 
-            console.log(searchVlue) 
-        }, 500); 
+        if (searchVlue.length <= 2) { 
+            setNewItems([{ id: 1, title: "You Shoud Add at least 3 Charachter for Showing data" }]) 
+        } else { 
+            timer = setTimeout(() => { 
+                fetch(`http://hn.algolia.com/api/v1/search?query=${searchVlue}&tags=story&hitsPerPage=50`) 
+                    .then(results => results.json()) 
+                    .then(data => { 
+                        let NewNews = [] 
+                        data.hits.map(item => { 
+                            let NewNew = { 
+                                id: item.objectID, 
+                                title: item.title, 
+                                author: item.author, 
+                                score: item.points, 
+                                time: item.created_at_i, 
+                                link: item.url, 
+                                Summary: item.story_text 
+                            } 
+                            // console.log(item) 
+                            NewNews.push(NewNew) 
+                        }) 
+                        if (NewNews.length == 0) { 
+                            NewNews.push({ id: Math.random, title: "There is no result match with your input" }) 
+                        } 
+                        setNewItems(NewNews) 
+                    }); 
+                // console.log(searchVlue) 
+            }, 500); 
+        } 
+ 
     } 
     return ( 
         <header className="header"> 
