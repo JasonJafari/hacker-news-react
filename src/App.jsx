27f0900385c8e6f416ebe0b8fd506dad358a53bb
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:25:35 -0400

    Add News to pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
  
 import './App.scss'; 
 import Header from './components/Header'; 
+import News from './components/News'; 
  
 const App = () => { 
   return ( 
     <div className="App"> 
       <Header /> 
+      <News /> 
     </div> 
    ); 
 } 
