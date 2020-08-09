d458c9387683a2cd1952d8e07206b9a84c92f1c2
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:24:48 -0400

    add News Component

---
+++ src/components/News.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; 
+ 
+import './News.scss'; 
+import New from './New'; 
+ 
+const News = () => { 
+    return ( 
+        <section class="news"> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+            <New /> 
+        </section> 
+ 
+    ); 
+} 
+ 
+export default News;
