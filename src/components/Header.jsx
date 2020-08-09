e739b2ba4be6d618c6185b77e2228cc2c643ab95
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:34:23 -0400

    Make handleSearch and console log the trget

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import './Header.scss' 
  
 const Header = () => { 
+    const handleSearch = (ev) =>{ 
+        console.log(ev.target.value) 
+    } 
     return ( 
         <header className="header"> 
             <h1>hacker news: </h1> 
-            <input type="text" placeholder="Please search" /> 
+            <input type="text" placeholder="Please search" onChange={handleSearch} /> 
         </header> 
     ); 
 } 
