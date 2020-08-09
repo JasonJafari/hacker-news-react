4f214d1a66a052d60184cebe5feaaaa1c3860598
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:48:58 -0400

    Change first context

--- src/shared/context/new-context.jsx
+++ src/shared/context/new-context.jsx
@@ -11,55 +11,9 @@ export const NewProvider = (props) => {
             score: 6015, 
             time: "2018-03-14T03:50:30.000Z", 
             link: "hhtp://", 
-            Summary: `abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP.` 
-        }, 
-        { 
-            id: 2, 
-            title: "Stephen Hawking has died", 
-            author: "Cogito", 
-            score: 6015, 
-            time: "2018-03-14T03:50:30.000Z", 
-            link: "hhtp://", 
-            Summary: `abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP.` 
-        }, 
-        { 
-            id: 3, 
-            title: "Stephen Hawking has died", 
-            author: "Cogito", 
-            score: 6015, 
-            time: "2018-03-14T03:50:30.000Z", 
-            link: "hhtp://", 
-            Summary: `abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP.` 
-        }, 
-        { 
-            id: 4, 
-            title: "Stephen Hawking has died", 
-            author: "Cogito", 
-            score: 6015, 
-            time: "2018-03-14T03:50:30.000Z", 
-            link: "hhtp://", 
-            Summary: `abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP.` 
-        }, 
-        { 
-            id: 5, 
-            title: "Stephen Hawking has died", 
-            author: "Cogito", 
-            score: 6015, 
-            time: "2018-03-14T03:50:30.000Z", 
-            link: "hhtp://", 
-            Summary: `abstraction: full stack engineer (I know some of you are rolling your eyes right now, just 
-                highlighting that I have experience on frontend apps as well as backend architecture). 
-                been working professionally for ~7 years building mostly javascript projects but also some PHP.` 
-        }, 
-]) 
+            Summary: null  
+        } 
+    ]) 
     return ( 
         <NewContext.Provider 
             value={[getNewItems, setNewItems]} 
