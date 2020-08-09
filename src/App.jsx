f3894295ef52a14278c8dd28a8672589911e16b2
Jason Jafari <me@jasonjafari.com>
2020-08-09 14:25:12 -0400

    Add footer to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react'; 
- 
-import './App.scss'; 
-import Header from './components/Header'; 
-import News from './components/News'; 
-import { NewProvider } from './shared/context/new-context'; 
-import Loader from './components/Loader'; 
-import { LoaderProvider } from './shared/context/loader-context'; 
- 
-const App = () => { 
-  return ( 
-    <NewProvider > 
-      <LoaderProvider > 
-        <div className="App"> 
-          <Header /> 
-          <News /> 
-          <Loader /> 
-        </div> 
-      </LoaderProvider> 
-    </NewProvider> 
-  ); 
-} 
- 
+import React from 'react';
+
+import './App.scss';
+import Header from './components/Header';
+import News from './components/News';
+import { NewProvider } from './shared/context/new-context';
+import Loader from './components/Loader';
+import { LoaderProvider } from './shared/context/loader-context';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <NewProvider >
+      <LoaderProvider >
+        <div className="App">
+          <Header />
+          <News />
+          <Loader />
+          <Footer />
+        </div>
+      </LoaderProvider>
+    </NewProvider>
+
+  );
+}
+
 export default App; 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss'
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Desigh by <a href="https://Jasonjafari.com" target="_blank">JasonJafari</a> Powered by React. Code is this <a href="https://github.com/JasonJafari/hacker-news-react" target="_blank">Repo</a> </p>
+        </footer>
+     );
+}
+ 
+export default Footer;
