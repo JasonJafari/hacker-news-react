30d9743ec6d700a9850bfc8038af6b2437b53550
Jason Jafari <me@jasonjafari.com>
2020-08-09 13:55:05 -0400

    make Loader context

---
+++ src/shared/context/loader-context.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react'; 
+ 
+export const LoaderContext = createContext(); 
+ 
+export const LoaderProvider = (props) => { 
+    const [getloader, setloader] = useState([ 
+        { 
+            value:0 
+        } 
+    ]) 
+    return ( 
+        <LoaderContext.Provider 
+            value={[getloader, setloader]} 
+        > 
+            {props.children} 
+        </LoaderContext.Provider> 
+    ) 
+}
